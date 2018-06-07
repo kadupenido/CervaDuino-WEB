5b004ce9027c632afdcdab4f03d336a0300b6472
Carlos Eduardo Campos Penido <contato@kadupenido.com>
2018-06-07 00:49:05 -0300

    ..

--- src/app/brassagem/brassagem-manual/brassagem-manual.component.ts
+++ src/app/brassagem/brassagem-manual/brassagem-manual.component.ts
@@ -19,8 +19,8 @@ export class BrassagemManualComponent implements OnInit {
     private spinnerService: Ng4LoadingSpinnerService,
     private vcr: ViewContainerRef,
     private toastr: ToastsManager) {
-      this.toastr.setRootViewContainerRef(vcr);
-      this.spinnerService.show();
+    this.toastr.setRootViewContainerRef(vcr);
+    this.spinnerService.show();
   }
 
   ngOnInit() {
@@ -29,6 +29,9 @@ export class BrassagemManualComponent implements OnInit {
     this.socket.on('connect', () => this.socketConnected(this.spinnerService, this.toastr));
     this.socket.on('connect_error', (err) => this.socketError(err, this.toastr));
     this.socket.on('reconnect_attempt', (attemptNumber) => this.socketReconnectAttempt(attemptNumber, this.spinnerService, this.toastr));
+    this.socket.on('boardIsReady', (ready) => this.boardIsReady(ready, this.toastr));
+
+    this.socket.emit('boardIsReady');
 
     this.dadosMostura = new DadosMostura();
   }
@@ -37,23 +40,31 @@ export class BrassagemManualComponent implements OnInit {
     this.dadosMostura = data;
   }
 
-   private socketConnected(spinnerService: Ng4LoadingSpinnerService, toastr: ToastsManager) {
+  private boardIsReady(ready, toastr: ToastsManager) {
+    if (ready) {
+      toastr.success('Circuito conectado', 'Arduino');
+    } else {
+      toastr.error('Falha no circuito', 'Arduino');
+    }
+  }
+
+  private socketConnected(spinnerService: Ng4LoadingSpinnerService, toastr: ToastsManager) {
     spinnerService.hide();
     toastr.success('Conexão estabelecida');
-   }
+  }
 
-   private socketError(error, toastr: ToastsManager) {
-     toastr.error(error.message, 'Falha ao conectar');
-   }
+  private socketError(error, toastr: ToastsManager) {
+    toastr.error(error.message, 'Falha ao conectar');
+  }
 
-   private socketReconnectAttempt(attemptNumber: number, spinnerService: Ng4LoadingSpinnerService, toastr: ToastsManager) {
-      spinnerService.show();
-      toastr.info('Tentativa: ' + attemptNumber, 'Reconectando');
-   }
+  private socketReconnectAttempt(attemptNumber: number, spinnerService: Ng4LoadingSpinnerService, toastr: ToastsManager) {
+    spinnerService.show();
+    toastr.info('Tentativa: ' + attemptNumber, 'Reconectando');
+  }
 
-   public dataChanged() {
-      console.log(this.dadosMostura);
-      this.socket.emit('data', this.dadosMostura);
-   }
+  public dataChanged() {
+    console.log(this.dadosMostura);
+    this.socket.emit('data', this.dadosMostura);
+  }
 
 }
