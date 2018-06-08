5b53105e00a01de834ab5ffe31295a3372998c23
Carlos Eduardo Campos Penido <contato@kadupenido.com>
2018-06-08 00:20:38 -0300

    remoçao socket

--- src/app/brassagem/brassagem-manual/brassagem-manual.component.ts
+++ src/app/brassagem/brassagem-manual/brassagem-manual.component.ts
@@ -24,47 +24,12 @@ export class BrassagemManualComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.socket = io.connect('http://localhost:3000');
-    this.socket.on('data', this.dataRecived);
-    this.socket.on('connect', () => this.socketConnected(this.spinnerService, this.toastr));
-    this.socket.on('connect_error', (err) => this.socketError(err, this.toastr));
-    this.socket.on('reconnect_attempt', (attemptNumber) => this.socketReconnectAttempt(attemptNumber, this.spinnerService, this.toastr));
-    this.socket.on('boardIsReady', (ready) => this.boardIsReady(ready, this.toastr));
-
-    this.socket.emit('boardIsReady');
-
     this.dadosMostura = new DadosMostura();
-  }
-
-  private dataRecived(data) {
-    this.dadosMostura = data;
-  }
-
-  private boardIsReady(ready, toastr: ToastsManager) {
-    if (ready) {
-      toastr.success('Circuito conectado', 'Arduino');
-    } else {
-      toastr.error('Falha no circuito', 'Arduino');
-    }
-  }
-
-  private socketConnected(spinnerService: Ng4LoadingSpinnerService, toastr: ToastsManager) {
-    spinnerService.hide();
-    toastr.success('Conexão estabelecida');
-  }
-
-  private socketError(error, toastr: ToastsManager) {
-    toastr.error(error.message, 'Falha ao conectar');
-  }
-
-  private socketReconnectAttempt(attemptNumber: number, spinnerService: Ng4LoadingSpinnerService, toastr: ToastsManager) {
-    spinnerService.show();
-    toastr.info('Tentativa: ' + attemptNumber, 'Reconectando');
+    this.spinnerService.hide();
   }
 
   public dataChanged() {
     console.log(this.dadosMostura);
-    this.socket.emit('data', this.dadosMostura);
   }
 
 }
